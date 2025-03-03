9b1d7c492fe52709a36a1a951b6c35ae6121dddd
ethanschmitt <ethanschmitt7@gmail.com>
2025-03-03 00:03:50 -0800

    Fix loss functions for batch accumulation

--- src/turbanet/loss.py
+++ src/turbanet/loss.py
@@ -13,11 +13,11 @@ __all__ = ["l2_loss", "softmax_cross_entropy"]
 
 def l2_loss(params: dict, batch: dict, apply_fn: Callable) -> tuple[ArrayImpl, ArrayImpl]:
     output = apply_fn({"params": params}, batch["input"])
-    loss = optax.l2_loss(predictions=output, targets=batch["output"])[0]
+    loss = optax.l2_loss(predictions=output, targets=batch["output"]).mean()
     return loss, output
 
 
-def softmax_cross_entropy(params: dict, apply_fn, batch: dict):  # noqa ANN201
+def softmax_cross_entropy(params: dict, batch: dict, apply_fn: Callable):  # noqa ANN201
     logits = apply_fn({"params": params}, batch["input"])
-    loss = jnp.mean(optax.softmax_cross_entropy(logits, batch["output"]))
+    loss = optax.softmax_cross_entropy(logits, batch["output"]).mean()
     return loss, logits
