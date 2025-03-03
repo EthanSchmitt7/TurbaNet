627d92ac5552cb7ef903987bf1b1957f0cbed5e1
ethanschmitt <ethanschmitt7@gmail.com>
2025-03-03 10:32:18 -0800

    Fix l2 test

--- test/integration/test_l2.py
+++ test/integration/test_l2.py
@@ -15,29 +15,28 @@ class Brain(nn.Module):
             x = nn.Dense(self.hidden_size)(x)
             x = nn.relu(x)
         x = nn.Dense(self.output_size)(x)
-        x = nn.softmax(x)
+        x = nn.sigmoid(x)
         return x
 
 
 # Network Parameters
-swarm_size = 1
-input_size = 3
+swarm_size = 10
+input_size = 9
 output_size = 1
 
 # Training Parameters
-learning_rate = 1e-6
-dataset_size = 8
-bootstrap_size = 8
-epochs = 1000
+learning_rate = 1e-3
+dataset_size = 10
+epochs = 10000
 
 # Generate a single batch input/output
 input = np.random.rand(dataset_size, input_size)
-output = np.random.rand(dataset_size, output_size)
+output = (np.random.rand(dataset_size, output_size) > 0.5) * 1.0
 
-# # Setup batch
+# Setup batch
 data = {
-    "input": np.repeat(input, swarm_size).reshape(swarm_size, input.shape[0], input.shape[1]),
-    "output": np.repeat(output, swarm_size).reshape(swarm_size, output.shape[0], output.shape[1]),
+    "input": np.repeat(np.expand_dims(input, 0), swarm_size, axis=0),
+    "output": np.repeat(np.expand_dims(output, 0), swarm_size, axis=0),
 }
 
 # Create networks
@@ -49,9 +48,13 @@ my_states1 = TurbaTrainState.swarm(
 )
 
 # Original loss
-loss, prediction = my_states1.check_loss(data["input"], data["output"], l2_loss)
-print(f"Initial loss | Mean: {np.mean(loss)} | Min: {np.min(loss)} | Max: {np.max(loss)}\n")
-
+initial_loss, initial_prediction = my_states1.check_loss(data["input"], data["output"], l2_loss)
+print(
+    f"Initial loss\n"
+    f"Mean: {np.mean(initial_loss):7.5} | "
+    f"Min: {np.min(initial_loss):7.5} | "
+    f"Max: {np.max(initial_loss):7.5}\n"
+)
 
 # Train for a while
 for i in range(epochs):
@@ -59,26 +62,23 @@ for i in range(epochs):
 
     if i % 100 == 0:
         print(
-            f"Epoch {i} | "
-            f"Mean loss: {np.mean(loss)} | "
-            f"Min loss: {np.min(loss)} | "
-            f"Max loss: {np.max(loss)}"
+            f"Epoch {i:>5} | "
+            f"Mean loss: {np.mean(loss):7.5} | "
+            f"Min loss: {np.min(loss):7.5} | "
+            f"Max loss: {np.max(loss):7.5}"
         )
 
-print(f"\nFinal loss | Mean: {np.mean(loss)} | Min: {np.min(loss)} | Max: {np.max(loss)}")
+print(
+    f"\nFinal loss\nMean: {np.mean(loss):7.5} | Min: {np.min(loss):7.5} | Max: {np.max(loss):7.5}"
+)
 
 # Merge the swarm and check the loss
 my_states2: TurbaTrainState = my_states1.merge()
 my_states2, prediction, loss = my_states2.train(
     np.expand_dims(input, 0), np.expand_dims(output, 0), l2_loss
 )
-print(f"\nMean of Weights: {loss[0]}")
-
-
-# Take the mean of their answers instead
-loss, prediction = my_states1.check_loss(data["input"], data["output"], l2_loss)
-print("Mean of Solutions: ", np.mean(loss))
+print(f"\nMean of Weights: {loss[0]:7.5}")
 
 # Take the mean of their answers instead
 loss, prediction = my_states1.check_loss(data["input"], data["output"], l2_loss)
-print("Mean of Solutions: ", np.mean(loss))
+print(f"Mean of Solutions: {np.mean(loss):7.5}")
