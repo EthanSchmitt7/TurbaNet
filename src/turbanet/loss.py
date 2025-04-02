2c5e71fe22386d8b46a66bb14466cc75f4f02adf
EthanSchmitt7 <66887006+EthanSchmitt7@users.noreply.github.com>
2025-04-02 09:46:28 -0700

    Release 0.3.0 (#5)
    * Update version
    
    * Add cross entropy loss
    
    * Have optimizer be an input to swarm
    
    * Update README
    
    * Reference Will Whitney
    
    * Remove horizontal bars
    
    * Update example
    
    * Update readme installation guide
    
    * Fix new installation guide
    
    * Set version number
    
    * Update tests

--- src/turbanet/loss.py
+++ src/turbanet/loss.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Callable
 
+import jax
+import jax.numpy as jnp
 import optax
 
 if TYPE_CHECKING:
@@ -24,6 +26,15 @@ def mse(
     prediction = apply_fn({"params": params}, input)
     loss = ((prediction - output) ** 2).mean()
     return loss, output
+
+
+def cross_entropy(
+    params: dict, input: ArrayImpl, output: ArrayImpl, apply_fn: Callable
+) -> tuple[ArrayImpl, ArrayImpl]:
+    log_probs = apply_fn({"params": params}, input)
+    labels = jax.nn.one_hot(output, log_probs.shape[1])
+    loss = -jnp.mean(jnp.sum(labels * log_probs, axis=-1))
+    return loss, labels
 
 
 def softmax_cross_entropy(params: dict, input: ArrayImpl, output: ArrayImpl, apply_fn: Callable):  # noqa ANN201
