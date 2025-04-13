5ff7690828e023e36d01795789eccc9182cfbe00
ethanschmitt <ethanschmitt7@gmail.com>
2025-04-12 18:05:43 -0700

    Make string comparison more exact

--- test/integration/test_gpu_support.py
+++ test/integration/test_gpu_support.py
@@ -1,7 +1,7 @@
 from jax.extend.backend import get_backend
 
 platform = get_backend().platform
-if platform != "GPU":
+if platform.lower() != "gpu":
     print(f"GPU support not available, using {platform}")
 else:
     print("GPU support available!")
